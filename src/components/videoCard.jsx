fb0f74154ceeb694673b8b21e91f7f6b6b8c0797
Schleidens <alectineschleidens2016@gmail.com>
2023-01-13 23:39:45 -0500

    added profile picture to video cards

--- src/components/videoCard.jsx
+++ src/components/videoCard.jsx
@@ -4,11 +4,17 @@ import { AiOutlineFire } from 'react-icons/ai'
 export default function trendingVideoCard({videoId, title, watch, time, channelName}) {
   return (
     <div className='mr-2.5 pb-4 hover:scale-125 ease-in duration-300  hover:bg-neutral-800 hover:rounded-md'>
+    <div>
     <img 
     src={`https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`}
      alt={title}
      className='h-auto w-full max-w-[full] md:max-w-[230px] rounded-md'
     />
+    <img 
+        src="https://avatars.githubusercontent.com/u/53914451?v=4"
+        alt="" 
+        className="rounded-full relative h-5 bottom-6 left-1" />
+    </div>
    
     <div className="flex flex-col px-2 mt-1">
         <div className="flex justify-between text-xs text-gray-400">
